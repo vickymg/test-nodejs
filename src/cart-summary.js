46085db6d633ebd2c76f156c2643fb1b9736b734
Vicky Gray <vicky.gray@digital.justice.gov.uk>
2016-12-22 17:29:35 +0000

    Test CartSummary returns the tax amount

--- src/cart-summary.js
+++ src/cart-summary.js
@@ -1,8 +1,10 @@
+var tax = require('./../src/tax');
+
 function CartSummary(items) {
   this._items = items;
 }
 
-CartSummary.prototype.getSubtotal = function() {
+CartSummary.prototype.getSubtotal = function () {
   if (this._items.length) {
     return this._items.reduce(function(subtotal, item) {
       return subtotal += (item.quantity * item.price);
@@ -11,5 +13,11 @@ CartSummary.prototype.getSubtotal = function() {
 
   return 0;
 };
+
+CartSummary.prototype.getTax = function (state, done) {
+  tax.calculate(this.getSubtotal(), state, function (taxInfo) {
+    done(taxInfo.amount);
+  });
+}
 
 module.exports = CartSummary;
--- test/cart-summary-test.js
+++ test/cart-summary-test.js
@@ -1,14 +1,16 @@
 var chai = require('chai');
 var expect = chai.expect;
+var sinon = require('sinon');
 var CartSummary = require('./../src/cart-summary');
+var tax = require('./../src/tax');
 
-describe('CartSummary', function() {
+describe('CartSummary', function () {
   it('getSubtotal() should return 0 if no items are passed in', function () {
     var cartSummary = new CartSummary([]);
     expect(cartSummary.getSubtotal()).to.equal(0);
   });
 
-  it('getSubtotal() should return the sum of price * quantity for all items', function() {
+  it('getSubtotal() should return the sum of price * quantity for all items', function () {
     var cartSummary = new CartSummary([{
       id: 1,
       quantity: 4,
@@ -25,4 +27,48 @@ describe('CartSummary', function() {
 
     expect(cartSummary.getSubtotal()).to.equal(300);
   });
+});
+
+// To stub out a method in Sinon, call the sinon.stub function and pass it the
+// object with the method being stubbed, the name of the method to be stubbed,
+// and a function that will replace the original during the test.
+
+describe('getTax', function () {
+  beforeEach(function () {
+    sinon.stub(tax, 'calculate', function(subtotal, state, done) {
+      // setTimeout is used to mimic the asynchronous behavior of this method
+      // since in reality it will make an asynchronous API call to a tax service. 
+      setTimeout(function () {
+        done({
+          amount: 30
+        });
+      }, 0);
+    });
+  });
+
+  afterEach(function () {
+    tax.calculate.restore();
+  });
+
+  it('getTax() should execute the callback function with the tax amount', function(done) {
+    var cartSummary = new CartSummary([{
+      id: 1,
+      quantity: 4,
+      price: 50
+    }, {
+      id: 2,
+      quantity: 2,
+      price: 30
+    }, {
+      id: 3,
+      quantity: 1,
+      price: 40
+    }]);
+
+    cartSummary.getTax('NY', function(taxAmount) {
+      expect(taxAmount).to.equal(30);
+      done();
+    });
+  });
+
 });
