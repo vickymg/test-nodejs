0ccb0dd2f61bad2fe2581681f8e0b3e78cdb05f6
Vicky Gray <vicky.gray@digital.justice.gov.uk>
2016-12-22 16:10:32 +0000

    Test CartSummary returns sum of price * quantity for all items

--- test/cart-summary-test.js
+++ test/cart-summary-test.js
@@ -7,4 +7,22 @@ describe('CartSummary', function() {
     var cartSummary = new CartSummary([]);
     expect(cartSummary.getSubtotal()).to.equal(0);
   });
+
+  it('getSubtotal() should return the sum of price * quantity for all items', function() {
+    var cartSummary = new CartSummary([{
+      id: 1,
+      quantity: 4,
+      price: 50
+    }, {
+      id: 2,
+      quantity: 2,
+      price: 30
+    }, {
+      id: 3,
+      quantity: 1,
+      price: 40
+    }]);
+
+    expect(cartSummary.getSubtotal()).to.equal(300);
+  });
 });
